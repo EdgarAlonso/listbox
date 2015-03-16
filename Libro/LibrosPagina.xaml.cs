3de4daee1483a2d7a308a513c1af5fabd2cd5e1b
Edgar Alonso Vidal Sarria <eavidal@unicauca.edu.co>
2015-03-16 12:04:58 -0500

    listbox

---
+++ Libro/LibrosPagina.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Libro.Resources;
+using Libro.Models;
+
+namespace Libro
+{
+    public partial class LibrosPagina : PhoneApplicationPage
+    {
+        public LibrosPagina()
+        {
+            InitializeComponent();
+        }
+
+        
+
+      
+
+        private void gotopage(object sender, SelectionChangedEventArgs e)
+        {
+            var x = lista.SelectedIndex;
+            if (x == 0) {
+                NavigationService.Navigate(new Uri("/MainPage.xaml?dato1=0", UriKind.Relative));
+            
+            }
+            if (x == 1)
+            {
+                NavigationService.Navigate(new Uri("/MainPage.xaml?dato1=1",UriKind.Relative));
+
+            }
+            if (x == 2)
+            {
+                NavigationService.Navigate(new Uri("/MainPage.xaml?dato1=2", UriKind.Relative));
+
+            }
+            }
+        }
+    } 
